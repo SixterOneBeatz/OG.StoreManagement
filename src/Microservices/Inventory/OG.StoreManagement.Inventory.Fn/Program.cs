785054e4314a9eabc17bbf582d655ceb15eb5928
Ronaldo Olmeda <ronaldo.olmeda@mobiik.com>
2024-03-11 12:00:15 -0600

    adding order and inventory functions

---
+++ src/Microservices/Inventory/OG.StoreManagement.Inventory.Fn/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OG.StoreManagement.IoC;
+using static OG.StoreManagement.IoC.DependencyContainer;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices((hostContext, services) =>
+    {
+        var configuration = hostContext.Configuration;
+
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddInventoryServices(configuration);
+    })
+    .Build();
+
+host.Run();
